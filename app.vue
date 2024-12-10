7e7219ddc45a1af613208f61f2270da680dac3e5
Andrii Dulkai <andrii.dulkai@right.link>
2024-12-10 16:37:47 +0200

    Add primevue theme

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <Button label="test"/>
   </div>
 </template>
---
+++ mytheme.ts
@@ -0,0 +1,27 @@
+import { definePreset } from '@primevue/themes';
+import Aura from '@primevue/themes/aura';
+
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{sky.50}',
+            100: '{sky.100}',
+            200: '{sky.200}',
+            300: '{sky.300}',
+            400: '{sky.400}',
+            500: '{sky.500}',
+            600: '{sky.600}',
+            700: '{sky.700}',
+            800: '{sky.800}',
+            900: '{sky.900}',
+            950: '{sky.950}'
+        }
+    }
+});
+
+export default {
+    preset: MyPreset,
+    options: {
+        darkModeSelector: '.p-dark'
+    }
+};
